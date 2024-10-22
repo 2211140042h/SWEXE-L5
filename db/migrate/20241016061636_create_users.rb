22bc1c4e414c03dcdad80929d9569044319e2758
Ubuntu <ubuntu@ip-172-31-89-166.ec2.internal>
2024-10-22 15:45:45 +0000

    push to render.com

--- db/migrate/20241016061636_create_users.rb
+++ db/migrate/20241016061636_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
-    create_table :users do |t|
+    create_table :users, if_not_exists: true do |t|
+#    create_table :users do |t|
       t.string :uid
       t.string :pass
       t.integer :age
